d61d566fe3d88ceb7a2963d3fd9a5eccfdb17a74
ShMcK <shawn.j.mckay@gmail.com>
2016-05-23 19:42:52 -0700

    logger minified

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -3,7 +3,6 @@ import assignTypes from './assign-types';
 export default function initProcessLogger(): void {
   if (console && console.log) {
     const originalLog = console.log;
-
     console.log = function(...args: any[]) {
       originalLog.apply(this, [assignTypes(args)]);
     };
