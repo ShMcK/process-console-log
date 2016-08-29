d2d0bbd437f40963eb41241a4f4c77dcaa5be06b
ShMcK <shawn.j.mckay@gmail.com>
2016-08-28 20:39:13 -0700

    export function rather than init process

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export {default as initProcessLogger} from './logger';
+import assignTypes from './logger/assign-types';
+
 export {default as parseLog} from './parse-log';
 export {default as logger} from './logger/logger-min';
+
+export default function setProcessLogger(): any {
+  if (console && console.log) {
+    const originalLog = console.log;
+    return function(...args: any[]) {
+      originalLog.apply(this, [assignTypes(args)]);
+    };
+  };
+}
